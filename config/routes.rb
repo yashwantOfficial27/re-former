627b6139cd1875d58c333c7c5b5cc56ffbf9997f
Yashwant Yadav <yadavyashwant.278@gmail.com>
2024-11-02 04:47:49 -0700

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  # get 'users/new'
+  # get 'users/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, :only => [:new, :create, :update, :edit]
+  # resources :users
 end
